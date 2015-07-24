5e2f74036af35f99a892ef4ac97308aed2bf796d
jotttt <jlaulik@hotmail.com>
2015-07-24 13:10:05 +0300

    Original

---
+++ public/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+jotttt:$1$9oFVaNPF$n494Mid09ixV1yPHmA0Tb.:Johann:jlaulik@gmail.com:admin,user
